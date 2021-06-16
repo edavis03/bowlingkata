61523ce23158a5296e3deec31abbfc7c24bd1fc4
Emily Davis <emilyjdavis94@gmail.com>
2021-06-15 22:36:12 -0400

    get kata rolling

--- src/main/java/BowlingGame.java
+++ src/main/java/BowlingGame.java
@@ -1,3 +1,26 @@
 public class BowlingGame {
+    private int rolls[] = new int[21];
+    private int rollNum = 0;
 
+    public void roll(int i) {
+        rolls[rollNum] = i;
+        rollNum++;
+    }
+
+    public int score() {
+        int gameScore = 0;
+        int i = 0;
+        for (int frame = 0; frame < 10; frame++) {
+            int frameScore;
+            if (rolls[i] + rolls[i + 1] == 10) {
+                frameScore = rolls[i] + rolls[i+1] + rolls[i+2];
+            }
+            else {
+                frameScore = rolls[i] + rolls[i+1];
+            }
+            gameScore += frameScore;
+            i = i + 2;
+        }
+        return gameScore;
+    }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -8,4 +8,35 @@ public class BowlingGameTest {
         assertThat(true).isTrue();
     }
 
+    @Test
+    public void noPinsEver() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            game.roll(0);
+        }
+
+        assertThat(game.score()).isEqualTo(0);
+    }
+
+    @Test
+    public void allOnes() {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0; i < 20; i++) {
+            game.roll(1);
+        }
+
+        assertThat(game.score()).isEqualTo(20);
+    }
+
+    @Test
+    public void oneSpare() {
+        BowlingGame game = new BowlingGame();
+        game.roll(7);
+        game.roll(3);
+        for (int i = 0; i < 17; i++) {
+            game.roll(1);
+        }
+
+        assertThat(game.score()).isEqualTo(28);
+    }
 }
