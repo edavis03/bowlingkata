f625e5fd51c432c06d4ba6d419114d68e7bb6d71
Emily Davis <emilyjdavis94@gmail.com>
2021-06-15 21:51:59 -0400

    set up kata

---
+++ src/test/java/BowlingGameTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BowlingGameTest {
+    @Test
+    public void trueIsTrue() {
+        assertThat(true).isTrue();
+    }
+
+}
